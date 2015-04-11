6bbe603c5d43b90858c76455c86fab862e8f064b
Stanthegithubman <sjrais01@louisville.edu>
2015-04-11 21:41:13 +0000

    more features added

--- machine-status.js
+++ machine-status.js
@@ -4,7 +4,7 @@ var firebase_sandbox ='https://spam.firebaseio.com'    //'firstbuild-sandbox.fir
 var os = require('os');
 var Firebase = require('firebase');
 var ref = new Firebase(firebase_sandbox);
-
+var userchk;
 //Sets User Table**************************************************************
 ref.child('Users').child('Jane').child('Uses').set(10,function(err){});
 ref.child('Users').child('Jane').child('BlueTooth Tag').set('x.123456.x',function(err){});
@@ -12,6 +12,8 @@ ref.child('Users').child('John').child('Uses').set(3,function(err){});
 ref.child('Users').child('John').child('BlueTooth Tag').set('x.123478.x',function(err){});
 ref.child('Users').child('Bill').child('Uses').set(2,function(err){});
 ref.child('Users').child('Bill').child('BlueTooth Tag').set('x.543216.x',function(err){});
+ref.child('Users').child('Ghost').child('Uses').set(0,function(err){});
+ref.child('Users').child('Ghost').child('BlueTooth Tag').set('x.XXXXXX.x',function(err){});
 //*******************************************************************************
 
 //Sets Up Current Machine Table************************************************
@@ -51,19 +53,19 @@ return item.address;
 
 function checkUser()
 {
-var userchk;
+//var userchk;
 ref.child('Current User').on("value", function(snapshot){userchk =snapshot.val();},function (errorObject){console.log("The read failed: "+errorObject.code);
 
-console.log("User Check Says:",userchk);//Troubleshooting
+//console.log("User Check Says:",userchk);//Troubleshooting
 
-	if(userchk!='Jane')
+/*	if(userchk!='Jane')
 	{
 	console.log("Its not Jane");
 	}
 	else
 	{
 	console.log("Its Jane");
-	}
+	}*/
 	ref.child('Current User').off();
 });
 
@@ -77,16 +79,16 @@ name.push(snap.key());
 //uses.push(snap.val()['uses']);
 //userarray.push({Username: name, Uses: uses});
 
-console.log(name[1]);
+//console.log(name[1]);
 //console.log(uses[1]);
 //console.log(userarray[1]);
 
 //ref.child('Users').push({Username: name, Uses: uses});
 var y = 0;
 /*for(x in peeps){
-	console.log(x)
+//	console.log(x)
 	userarray[y] = x;
-	console.log(userarray[y]);
+//	console.log(userarray[y]);
 	y++;
 }*/
 for(y=0; y<name.length;y++)
@@ -101,7 +103,7 @@ for(y=0; y<name.length;y++)
 	}
 }
 
-console.log("GTG:",GTG);
+//console.log("GTG:",GTG);
 });
 return(1);//GTG);
 }//END OF USERCHK FUNCTION******************************************************
@@ -117,17 +119,26 @@ if(chekd == wreked)
 value = 1;
 var authuser;
 ref.child('Current User').on("value", function(snapshot){authuser =snapshot.val();},function (errorObject){console.log("The read failed: "+errorObject.code)});
-         var contentchg ={'Uses': 9};
-	 ref.child('Users').child(authuser).update(contentchg,function(err){});
+         //var contentchg ={'Uses': 9};
+	 //ref.child('Users').child(authuser).update(contentchg,function(err){});
 }
 else
 {
 value = 0;
 }
-console.log("Get Wrcked!",value);
+inc = getvalue(userchk);
+//console.log("Get Wrcked!",value);
 return(value);
 }//END COMPARE**************************************************************
 
+//Get value Function********************************************************
+function getvalue(usname)
+{
+var custom=0;
+ref.child('Users').child('usname').on("value",function(snapshot){custom = snapshot.val();},function(err){});
+
+return(custom);
+}
 //BEGIN MAIN??******************************************************************
 //Setup the Green Bean
 var greenBean = require("green-bean");
@@ -137,7 +148,7 @@ ref.child('Available Machines').child('Dryer 1').child('Green Bean').update(ipco
 
 var result = 0;
 result = compare(checkUser(),1);
-console.log("Result:",result);
+//console.log("Result:",result);
 
 
 
@@ -147,9 +158,11 @@ console.log("Result:",result);
 
 
 var inc =1;
- var watcher = 0;
-
-
+var watcher = 0;
+inc = getvalue(userchk);
+var contentchg ={'Uses': inc};
+var runs = {'Times Used':run};
+var run = 0;
 //Finish Cycle and charge account * Started Below *
       // subscribe to the end of cycle
       console.log("Subscribing to machineStatus.");
@@ -166,6 +179,8 @@ var inc =1;
           content ={'Machine Status': value};
           ref.child('Available Machines').child('Dryer 1').update(content, function(err){});
 console.log("Watcher is: ",watcher);
+//LOGIC CODE FOR INCREMENTING**************************************************
+//*****************************************************************************
 if (value ==2&&watcher ==0) 
 {
 watcher = 1;
@@ -173,15 +188,30 @@ inc++;
 console.log("Watcher 1");
 console.log(value," ",watcher," ",result);
 }
+
+else if(value == 3 && watcher == 1 &&result ==0) //Ghost Catcher
+{
+watcher = 0;
+contentchg ={'Uses': inc};
+ref.child('Users').child('Ghost').update(contentchg,function(err){});
+runs = {'Times Used':run};
+ref.child('Available Machines').child('Dryer 1').child('Machine Usage Table').child('2015').child('April').child('11').update(runs, function(err){});
+result = compare(checkUser(),1);
+}
+
 else if(value == 3 &&watcher ==1&&result ==1)
 {
  
 watcher =0;
 var authuser;
 ref.child('Current User').on("value", function(snapshot){authuser =snapshot.val();},function (errorObject){console.log("The read failed: "+errorObject.code)});
-         var contentchg ={'Uses': inc};
-	 ref.child('Users').child('Bill').update(contentchg,function(err){});
-console.log("Triggered!");
+         contentchg ={'Uses': inc};
+	 ref.child('Users').child(authuser).update(contentchg,function(err){});
+console.log("Triggered!",inc);
+run++;
+runs = {'Times Used':run};
+ref.child('Available Machines').child('Dryer 1').child('Machine Usage Table').child('2015').child('April').child('11').update(runs, function(err){});
+result = compare(checkUser(),1);
 }
 
 	 });
